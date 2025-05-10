666f4a7fe517af7acb3612466770dc75d47b78a6
Martijn van der Ven <martijn@vanderven.se>
2025-05-10 20:45:06 +0200

    Add files from extension v0.9.0 (14 Oct 2024)

--- options.js
+++ options.js
@@ -5,12 +5,31 @@ function save_options() {
     var bPageNew = document.getElementById('cbPageNew').checked;
     var bArchiveNew = document.getElementById('cbArchiveNew').checked;
     var bSearchNew = document.getElementById('cbSearchNew').checked;
+    var bNotify = document.getElementById('cbNotify').checked;
+
+    // request permission for notifications when the Notify option is checked
+    if (bNotify) {
+        chrome.permissions.request({
+            permissions: ['notifications'] }, (granted) => {
+            // The callback argument will be true if the user granted the permissions.
+            if (granted) {
+                // do nothing
+            } else {
+                bNotify = false;
+                document.getElementById('cbNotify').checked = false;
+            }
+        });                    
+    } else {
+        chrome.permissions.remove({ permissions: ['notifications'] })
+    }
+
     chrome.storage.sync.set({
         tabOption: iTabOption,
         activateButtonNew: bButtonNew,
         activatePageNew: bPageNew,
         activateArchiveNew: bArchiveNew,
-        activateSearchNew: bSearchNew
+        activateSearchNew: bSearchNew,
+        notifyOption: bNotify
     }, function() {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
@@ -25,13 +44,14 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
     // Default tab option is Adjacent (0, 1: end, 2: active)
-    // Use default value true for all activate options.
+    // defaults values for all activate options.
     chrome.storage.sync.get({
         tabOption: 0,
         activateButtonNew: true,
         activatePageNew: true,
-        activateArchiveNew: true,
-        activateSearchNew: true
+        activateArchiveNew: false,
+        activateSearchNew: true,
+        notifyOption: false
     }, function(items) {
         switch (items.tabOption) {
             case 1:
@@ -47,7 +67,9 @@ function restore_options() {
         document.getElementById('cbPageNew').checked = items.activatePageNew;
         document.getElementById('cbArchiveNew').checked = items.activateArchiveNew;
         document.getElementById('cbSearchNew').checked = items.activateSearchNew;
+        document.getElementById('cbNotify').checked = items.notifyOption;
     });
 }
+
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('bSave').addEventListener('click', save_options);
